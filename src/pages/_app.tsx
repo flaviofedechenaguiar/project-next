209968616f6d3aee3df267cc2dd387bd0534ac63
flavio aguiar <flaviofedechenaguiar@gmail.com>
2021-12-21 10:41:23 -0300

    feat: add Header and Footer component in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.scss";
 import { AppProps } from "next/app";
+import Head from "next/head";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <title>Gazin Films</title>
+      </Head>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 };
