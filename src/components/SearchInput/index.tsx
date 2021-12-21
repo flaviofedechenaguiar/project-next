2187f27ffdddbe4ea55863d2ceda89fc17ab8306
flavio aguiar <flaviofedechenaguiar@gmail.com>
2021-12-21 09:44:41 -0300

    feat: add code of manipulate route and input in SearchInput component

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,13 +1,36 @@
 import Image from "next/image";
-
+import { useRouter } from "next/router";
+import { useState } from "react";
 import styles from "./SearchInput.module.scss";
 
+interface Inputs {
+  search: string;
+}
+
 export const SearchInput: React.FC = () => {
+  const [input, setInput] = useState<Inputs>({ search: "" });
+  const router = useRouter();
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setInput((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleRedirect = () => {
+    const URL_SEARCH = "/search";
+    router.push(`${URL_SEARCH}?search=${input.search}`);
+  };
+
   return (
     <>
       <div className={styles["search-input"]}>
-        <input></input>
-        <button>
+        <input
+          name="search"
+          id="search"
+          onChange={handleOnChange}
+          value={input.search}
+        ></input>
+        <button onClick={handleRedirect}>
           <Image
             src="/icons/search-icon.svg"
             alt="icone de lupa"
