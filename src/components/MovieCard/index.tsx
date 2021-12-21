9021ead161b28fdb2bc717320963dce1bd00d82c
flavio aguiar <flaviofedechenaguiar@gmail.com>
2021-12-21 09:46:24 -0300

    feat: add codition of no image and correct Props in MovieCard component

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,25 +1,51 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import styles from "./MovieCard.module.scss";
 
 interface Props {
   name: string;
-  year: string;
-  imageUrl: string;
+  year: number;
+  imageUrl: string | null | undefined;
+  redirectLink: string;
 }
 
-export const MovieCard: React.FC<Props> = ({ name, year, imageUrl }) => {
+export const MovieCard: React.FC<Props> = ({
+  name,
+  year,
+  imageUrl,
+  redirectLink,
+}) => {
   return (
     <>
-      <div className={styles["movie-card"]}>
-        <div className={styles["movie-card__image-wrapper"]}>
-          <Image alt={name} src={imageUrl} layout="fill" objectFit="contain" />
-        </div>
-        <div className={styles["movie-card__wrapper-text"]}>
-          <h4>{name}</h4>
-          <span>{year}</span>
-        </div>
-      </div>
+      <Link href={redirectLink}>
+        <a>
+          <div className={styles["movie-card"]}>
+            <div className={styles["movie-card__image-wrapper"]}>
+              {!imageUrl ? (
+                <Image
+                  alt={"no image"}
+                  src={"/no-image.png"}
+                  layout="fill"
+                  objectFit="cover"
+                />
+              ) : null}
+              {imageUrl ? (
+                <Image
+                  alt={name}
+                  src={imageUrl}
+                  layout="fill"
+                  objectFit="cover"
+                />
+              ) : null}
+            </div>
+            <div className={styles["movie-card__wrapper-text"]}>
+              <h4>{name}</h4>
+              <span>{year}</span>
+            </div>
+          </div>
+        </a>
+      </Link>
     </>
   );
 };
