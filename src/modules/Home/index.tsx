2a202cfba689e2d5f61d212469d0d9ea19ef2320
flavio aguiar <flaviofedechenaguiar@gmail.com>
2021-12-21 09:59:09 -0300

    feat: create Home module

---
+++ src/modules/Home/index.tsx
@@ -0,0 +1,66 @@
+import { useEffect } from "react";
+import { Banner } from "../../components/Banner";
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { ListActors } from "../../components/ListActors";
+import { ListFilms } from "../../components/ListFilms";
+
+import { useActors } from "../../hooks/useActors";
+import { useFilms } from "../../hooks/useFilms";
+
+import styles from "./Home.module.scss";
+
+export const Home: React.FC = () => {
+  const { getAllActors, addActorIntoActors, actors } = useActors();
+  const {
+    getAllFilms,
+    getAllReleases,
+    getBanners,
+    films,
+    banners,
+    releaseFilms,
+    addFilmIntoFilms,
+    addFilmIntoFilmsRelease,
+  } = useFilms();
+
+  useEffect(() => {
+    getAllFilms();
+    getAllReleases();
+    getAllActors();
+    getBanners();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main className={styles["home"]}>
+        <Banner banners={banners} />
+        <div className={styles["home__popular-films-wrapper"]}>
+          <ListFilms
+            id="popular-list"
+            title="Filmes populares"
+            films={films}
+            onChangePosition={addFilmIntoFilms}
+          />
+        </div>
+        <div className={styles["home__next-releases-wrapper"]}>
+          <ListFilms
+            id="next-releases-list"
+            title="Próximos lançamentos"
+            films={releaseFilms}
+            onChangePosition={addFilmIntoFilmsRelease}
+          />
+        </div>
+        <div className={styles["home__popular-actors-wrapper"]}>
+          <ListActors
+            id="actor-list"
+            title="Atores populares"
+            actors={actors}
+            onChangePosition={addActorIntoActors}
+          />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import { Home as HomePage } from "../modules/Home";
+
 const Home: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <HomePage />
+
+      {/* <Banner /> */}
+      {/* <Header /> */}
+      {/* <MovieCard
+        imageUrl="/movie-card-exemple.png"
+        name="testesdfsdfsdfsdfsdffdsf"
+        year="2014"
+      /> */}
+      {/* <ActorCard
+        imageUrl="/movie-card-exemple.png"
+        name="Rober Pig"
+        quantityMovies={20}
+      /> */}
+      {/* <ListFilms /> */}
+      {/* <ListActors /> */}
+      {/* <Footer /> */}
+    </>
+  );
 };
 
 export default Home;
